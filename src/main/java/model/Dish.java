88a10db28cfef7d496a4858e5245f096f0b4b2c6
Wouter Bos <wouwouwou@users.noreply.github.com>
2015-12-08 15:42:00 +0100

    Added javadoc to the dish class

--- src/main/java/model/Dish.java
+++ src/main/java/model/Dish.java
@@ -2,38 +2,81 @@ package model;
 
 /**
  * Created by Wouter on 8-12-2015.
+ *
+ * This class represents a Dish.
  */
 public class Dish {
 
+    /**
+     * The title of the Dish.
+     */
     private String title;
+
+    /**
+     * The description of the Dish.
+     */
     private String description;
+
+    /**
+     * The grade of the Dish.
+     */
     private double grade;
 
+    /**
+     * Constructor of a Dish.
+     * @param title The dish needs a title.
+     * @param description The dish needs a description.
+     */
     public Dish(String title, String description) {
         this.title = title;
         this.description = description;
+        this.grade = 0.0;
     }
 
+    /**
+     * Title getter.
+     * @return The title of this dish.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Title setter.
+     * @param title The new title of this dish.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Description getter.
+     * @return The description of this dish.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Description setter.
+     * @param description The new description of this dish.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Grade getter.
+     * @return The grade of this dish.
+     */
     public double getGrade() {
         return grade;
     }
 
+    /**
+     * Grade setter.
+     * @param grade The new grade of this dish.
+     */
     public void setGrade(double grade) {
         this.grade = grade;
     }
