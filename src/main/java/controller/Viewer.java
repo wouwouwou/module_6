c1f187e60f66bc6ada9e5cff2b8f06a77d7cfc3f
Wouter Bos <wouwouwou@users.noreply.github.com>
2015-12-14 10:29:40 +0100

    Javadoc

--- src/main/java/controller/Viewer.java
+++ src/main/java/controller/Viewer.java
@@ -2,6 +2,8 @@ package controller;
 
 /**
  * Created by Wouter on 14-12-2015.
+ *
+ * This class controls the model as a Viewer.
  */
 public class Viewer {
 
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -4,4 +4,7 @@ package view;
  * Created by Wouter on 8-12-2015.
  */
 public class View {
+
+
+
 }
