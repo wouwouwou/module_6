f1943405793b37982516db5603e312698fc5198c
Wouter Bos <wouwouwou@users.noreply.github.com>
2015-12-14 14:45:13 +0100

    Trying to get travis working. :)

--- src/test/java/model/DishTest.java
+++ src/test/java/model/DishTest.java
@@ -14,6 +14,8 @@ public class DishTest {
     @Before
     public void setUp() throws Exception {
 
+        Dish dish1 = new Dish("Dame Blanche", "Vanille ijs met chocoladesaus");
+
     }
 
     @Test
