0edf567b4513e9d32e5153b4ece191151bea52e7
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-01-05 14:42:24 +0100

    JSONTools class. :)

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -121,78 +121,9 @@ public class Controller {
      */
     public void exportMenu() throws IOException {
         JSONObject menu = new JSONObject();
-        JSONArray dishes = dishesToJSONArray();
+        JSONArray dishes = JSONTools.dishesToJSONArray(this.menu);
         menu.put("dishes", dishes);
-        saveJSON("menu.json", menu);
-    }
-
-    /**
-     * Makes a JSONArray of all the dishes on the menu.
-     * @return JSONArray of all the dishes on the menu.
-     */
-    private JSONArray dishesToJSONArray() {
-        JSONArray dishes = new JSONArray();
-        ArrayList<Dish> dishlist = getMenu().getDishes();
-        for (Dish d : dishlist) {
-            JSONObject dish = dishToJSONObject(d);
-            dishes.add(dish);
-        }
-        return dishes;
-    }
-
-    /**
-     * Makes a JSONObject of a dish on the menu.
-     * @param d the dish on the menu
-     * @return the dish on the menu as JSONObject
-     */
-    private JSONObject dishToJSONObject(Dish d) {
-        JSONObject dish = new JSONObject();
-        dish.put("id", d.getId());
-        dish.put("title", d.getTitle());
-        dish.put("description", d.getDescription());
-        dish.put("imgpath", d.getImgpath());
-        dish.put("comments", commentsToJSONArray(d));
-        dish.put("grades", gradesToJSONArray(d));
-        return dish;
-    }
-
-    /**
-     * Makes a JSONArray of the grades of a dish
-     * @param d the dish
-     * @return the grades as JSONArray
-     */
-    private JSONArray gradesToJSONArray(Dish d) {
-        JSONArray grades = new JSONArray();
-        for (int g : d.getGrades()) {
-            grades.add(g);
-        }
-        return grades;
-    }
-
-    /**
-     * Makes a JSONArray of the comments on a dish
-     * @param d the dish
-     * @return the comments as JSONArray
-     */
-    private JSONArray commentsToJSONArray(Dish d) {
-        JSONArray comments = new JSONArray();
-        for (String c : d.getComments()) {
-            comments.add(c);
-        }
-        return comments;
-    }
-
-    /**
-     * Saves a JSONObject to a given filename.
-     * @param filename the filename
-     * @param json the JSONObject to be saved
-     * @throws IOException
-     */
-    private void saveJSON(String filename, JSONObject json) throws IOException {
-        FileWriter file = new FileWriter(filename);
-        file.write(json.toJSONString());
-        file.flush();
-        file.close();
+        JSONTools.saveJSON("menu.json", menu);
     }
 
     public Menu getMenu() {
