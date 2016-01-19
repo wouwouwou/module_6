84ec233f7a59c1a2f3dc29342cd0a0cfd3a4893d
wouwouwou <wouter_bos96@hotmail.com>
2016-01-19 18:50:51 +0100

    Code improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
         Controller c = new Controller();
         c.getMenu().getDishes().clear();
-        Dish d1 = new Dish(1, "Baked Saithe Fillet", "Baked Saithe Fillet with a tartar sauce, carrots and French fries.", new ArrayList<Integer>(), new ArrayList<String>(), "fish.jpg");
-        Dish d2 = new Dish(2, "Belgian Beef Stew", "Belgian beef stew with ratatoulle and potato gratin.", new ArrayList<Integer>(), new ArrayList<String>(), "stew.jpg");
-        Dish d3 = new Dish(3, "Mozzarella Pie", "Mozzarella pie with Hollandaise sauce, carrots and fries.", new ArrayList<Integer>(), new ArrayList<String>(), "pie.jpg");
+        Dish d1 = new Dish(1, "Baked Saithe Fillet", "Baked Saithe Fillet with a tartar sauce, carrots and French fries.", new ArrayList<>(), new ArrayList<>(), "fish.jpg");
+        Dish d2 = new Dish(2, "Belgian Beef Stew", "Belgian beef stew with ratatoulle and potato gratin.", new ArrayList<>(), new ArrayList<>(), "stew.jpg");
+        Dish d3 = new Dish(3, "Mozzarella Pie", "Mozzarella pie with Hollandaise sauce, carrots and fries.", new ArrayList<>(), new ArrayList<>(), "pie.jpg");
         c.getMenu().getDishes().add(d1);
         c.getMenu().getDishes().add(d2);
         c.getMenu().getDishes().add(d3);
