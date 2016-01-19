6a5ce651b22303693eae3c6862a19c76fd2b8394
wouwouwou <wouter_bos96@hotmail.com>
2016-01-19 18:50:03 +0100

    Better version. :D

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import controller.Controller;
+import model.Dish;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Wouter on 6-1-2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Controller c = new Controller();
+        c.getMenu().getDishes().clear();
+        Dish d1 = new Dish(1, "Baked Saithe Fillet", "Baked Saithe Fillet with a tartar sauce, carrots and French fries.", new ArrayList<Integer>(), new ArrayList<String>(), "fish.jpg");
+        Dish d2 = new Dish(2, "Belgian Beef Stew", "Belgian beef stew with ratatoulle and potato gratin.", new ArrayList<Integer>(), new ArrayList<String>(), "stew.jpg");
+        Dish d3 = new Dish(3, "Mozzarella Pie", "Mozzarella pie with Hollandaise sauce, carrots and fries.", new ArrayList<Integer>(), new ArrayList<String>(), "pie.jpg");
+        c.getMenu().getDishes().add(d1);
+        c.getMenu().getDishes().add(d2);
+        c.getMenu().getDishes().add(d3);
+        try {
+            c.exportMenu();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
